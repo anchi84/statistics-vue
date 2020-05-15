a2233346d48e8bd67cb2d26f538c94187f88f0eb
anarili <anarili@gmail.com>
2020-05-15 18:27:12 +0200

    routes refactored

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,15 @@ const routes = [
     // this generates a separate chunk (statistics.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "statistics" */ '../views/Statistics.vue')
+  },
+  {
+    path: '/404',
+    name: 'NotFound',
+    component: () => import(/* webpackChunkName: "404" */ '@/views/NotFound.vue')
+  },
+  {
+    path: '*',
+    redirect: '/404'
   }
 ]
 
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is the statistics page</h1>
     <div class="chart-wrapper">
-      <chart :options="chartOptionsLine"></chart>
+      <chart :options="chartOptionsLine" autoresize></chart>
     </div>
   </div>
 </template>
@@ -31,25 +31,34 @@ export default {
       }
     }
   },
-  created () {
+  mounted () {
     const dataValuesFields = []
-    Object.values(this.$route.params.appGetData).forEach((field, index) => {
-      let time = 0
-      const dataValuesField = []
-      field.forEach(element => {
-        const coordinates = [element, time]
-        dataValuesField.push(coordinates)
-        time += 2
+    if (this.$route.params.appGetData) {
+      Object.values(this.$route.params.appGetData).forEach((field, index) => {
+        let time = 0
+        const dataValuesField = []
+        field.forEach(element => {
+          const coordinates = [element, time]
+          dataValuesField.push(coordinates)
+          time += 2
+        })
+        dataValuesFields.push(dataValuesField)
+        const seriesObj = {
+          data: dataValuesField,
+          type: 'line',
+          color: '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6)
+        }
+        this.chartOptionsLine.series.push(seriesObj)
       })
-      dataValuesFields.push(dataValuesField)
-      const seriesObj = {
-        data: dataValuesField,
-        type: 'line',
-        color: '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6)
+      console.log(dataValuesFields)
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      if (!vm.$route.params.appGetData) {
+        vm.$router.push('/')
       }
-      this.chartOptionsLine.series.push(seriesObj)
     })
-    console.log(dataValuesFields)
   }
 }
 </script>
